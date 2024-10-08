0faed3b8866142a6376400946c71ee08915d7e25
nan-0125 <xz985598341a@gmail.com>
2024-10-08 15:21:46 +0800

    feat: 路由和注册

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import App from './App.tsx'
+// import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import BookManage from './pages/BookManage';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const routes = [
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/",
+    element: <BookManage/>,
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+    <RouterProvider router={router} />
+  // </StrictMode>,
 )
