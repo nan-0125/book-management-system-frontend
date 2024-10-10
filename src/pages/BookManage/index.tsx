1f0e1aa8b0660534973fbd87918b859ad6089bf4
nan-0125 <xz985598341a@gmail.com>
2024-10-10 16:56:53 +0800

    feat: 添加&更新

--- src/pages/BookManage/index.tsx
+++ src/pages/BookManage/index.tsx
@@ -1,7 +1,9 @@
-import {Button, Card, Form, Input, message} from "antd";
+import { Button, Card, Form, Input, message } from "antd";
 import "./index.css";
-import {useCallback, useEffect, useState} from "react";
-import {InternetError, list} from "../../services";
+import { useEffect, useState } from "react";
+import { InternetError, list } from "../../services";
+import { CreateBookModal } from "./CreateBookModal";
+import UpdateBookModal from "./UpdateBookModal";
 
 interface Book {
   id: number;
@@ -13,73 +15,91 @@ interface Book {
 
 export default function BookManage() {
   const [bookList, setBookList] = useState<Array<Book>>([]);
-  const [name, setName] = useState("");
-
-  const fetchData = useCallback(async function() {
-    try {
-      const data = await list(name);
-
-      if(data.status === 201 || data.status === 200) {
-        setBookList(data.data);
-      }
-    } catch(e) {
-      const err = e as InternetError;
-      message.error(err.response?.data.message);
-    }
-  }, [name])
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+  const [isUpdateBookModalOpen, setIsUpdateBookModalOpen] = useState(false);
+  const [updateListCount, setUpdateListCount] = useState(0);
+  const [name, setName] = useState('');
+  const [updateId, setUpdateId] = useState(0);
 
   async function searchBook(values: { name: string; author: string }) {
-    setName(values.name);
+    setName(values.name ?? '');
   }
 
   useEffect(() => {
+    async function fetchData () {
+      try {
+        const data = await list(name);
+  
+        if (data.status === 201 || data.status === 200) {
+          setBookList(data.data);
+        }
+      } catch (e) {
+        const err = e as InternetError;
+        message.error(err.response?.data.message);
+      }
+    }
     fetchData();
-  }, [fetchData]);
+  }, [name, updateListCount]);
+
   return (
-      <div id="bookManage">
-        <h1>图书管理系统</h1>
-        <div className="content">
-          <div className='book-search'>
-            <Form
-                name="search"
-                layout='inline'
-                colon={false}
-                onFinish={searchBook}
-            >
-              <Form.Item label="图书名称" name="name">
-                <Input/>
-              </Form.Item>
-              <Form.Item label=" ">
-                <Button type="primary" htmlType="submit">
-                  搜索图书
-                </Button>
-                <Button type="primary" htmlType="submit" style={{background: 'green'}}>
-                  添加图书
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
-          <div className="book-list">
-            {
-              bookList.map(book => {
-                return <Card
-                    className='card'
-                    hoverable
-                    style={{width: 300}}
-                    cover={<img alt="example" src={`http://localhost:3000/${book.cover}`}/>}
-                >
-                  <h2>{book.name}</h2>
-                  <div>{book.author}</div>
-                  <div className='links'>
-                    <a href="#">详情</a>
-                    <a href="#">编辑</a>
-                    <a href="#">删除</a>
-                  </div>
-                </Card>
-              })
-            }
-          </div>
+    <div id="bookManage">
+      <CreateBookModal isOpen={isCreateModalOpen} handleClose={() => {
+        setIsCreateModalOpen(false);
+        setUpdateListCount((pre) => pre + 1);
+      }} />
+      <UpdateBookModal isOpen={isUpdateBookModalOpen} id={updateId} handleClose={() => {
+        setIsUpdateBookModalOpen(false);
+        setUpdateListCount((pre) => pre + 1);
+      }} />
+      <h1>图书管理系统</h1>
+      <div className="content">
+        <div className='book-search'>
+          <Form
+            name="search"
+            layout='inline'
+            colon={false}
+            onFinish={searchBook}
+          >
+            <Form.Item label="图书名称" name="name">
+              <Input />
+            </Form.Item>
+            <Form.Item label=" ">
+              <Button type="primary" htmlType="submit">
+                搜索图书
+              </Button>
+            </Form.Item>
+            <Button type="primary" htmlType="submit" style={{ background: 'green' }} onClick={() => {
+                setIsCreateModalOpen(true);
+              }}>
+                添加图书
+              </Button>
+          </Form>
+        </div>
+        <div className="book-list">
+          {
+            bookList.map(book => {
+              return <Card
+                key={book.id}
+                className='card'
+                hoverable
+                style={{ width: 300 }}
+                cover={<img alt="example" src={`http://localhost:3000/${book.cover}`} />}
+              >
+                <h2>{book.name}</h2>
+                <div>{book.author}</div>
+                <div className='links'>
+                  <a href="#">详情</a>
+                  <a onClick={() => {
+                    setUpdateId(book.id);
+                    setIsUpdateBookModalOpen(true);
+                  }}>编辑</a>
+                  <a href="#">删除</a>
+                </div>
+              </Card>
+            })
+          }
         </div>
       </div>
+    </div>
   )
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import type {AxiosError} from "axios";
+import { CreateBook } from "../pages/BookManage/CreateBookModal";
+import { UpdateBook } from "../pages/BookManage/UpdateBookModal";
 
 export type InternetError = AxiosError<{message:string}>;
 
@@ -38,5 +40,38 @@ export async function list(name: string) {
     params: {
       name
     }
+  });
+}
+
+/**
+ * 新建图书
+ * @param book 图书信息
+ * @returns 
+ */
+export async function create(book: CreateBook) {
+  return await axiosInstance.post('/book/create', book)
+}
+
+/**
+ * 图书详细信息
+ * @param id 图书id
+ * @returns 图书详细信息
+ */
+export async function detail(id: number) {
+  return await axiosInstance.get(`/book/${id}`);
+}
+
+/**
+ * 更新图书信息
+ * @param book 图书信息
+ * @returns 
+ */
+export async function update(book: UpdateBook) {
+  return await axiosInstance.put('/book/update', {
+      id: book.id,
+      name: book.name,
+      author: book.author,
+      description: book.description,
+      cover: book.cover
   });
 }
